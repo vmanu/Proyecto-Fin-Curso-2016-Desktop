601aee54e22328ff03b9118e6e7036e1c6c99764
ivanPellisso <ivanpellisso.red@gmail.com>
2016-04-28 19:22:20 +0200

    19:22

---
+++ DataPPTGame/src/main/java/com/mycompany/datapptgame/User.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.datapptgame;
+
+/**
+ *
+ * @author IvPR
+ */
+public class User {
+    private String login;
+    private String pass;
+
+    public User(String login, String pass) {
+        this.login = login;
+        this.pass = pass;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "login=" + login + ", pass=" + pass + '}';
+    }
+    
+    
+}
