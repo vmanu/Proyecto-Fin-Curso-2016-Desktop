b0021a88c3e21290b845f39a739ba82e33b81f95
vmanu <vmanuoveuh@hotmail.com>
2016-05-07 18:07:17 +0200

    12:36

--- DesktopApp/src/main/java/utilities/MyClientEndpoint.java
+++ DesktopApp/src/main/java/utilities/MyClientEndpoint.java
@@ -43,7 +43,7 @@ public class MyClientEndpoint extends Endpoint {
 
     public MyClientEndpoint(final DataContainer datos) {
         try {
-            //192.168.206.1 PORTATIL - 192.168.1.104 CASA
+            //192.168.206.1 PORTATIL - 192.168.1.104 CASA - SERVIDOR (DEFINITIVO) ws://servidor-pptgame.rhcloud.com:8000/ServerPPTGame/ppt?user=
             URI uri = new URI("ws://192.168.1.104:8080/ServerPPTGame/ppt?user=" + datos.getNombreJ1());
             connectToWebSocket(uri);
         } catch (URISyntaxException ex) {
