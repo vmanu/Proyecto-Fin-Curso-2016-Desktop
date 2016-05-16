2075bba3c7b9d38d06e48af84536ebc3db77a244
vmanu <vmanuoveuh@hotmail.com>
2016-05-16 12:58:42 +0200

    17:42

--- DesktopApp/src/main/java/vista/FXMLLoginController.java
+++ DesktopApp/src/main/java/vista/FXMLLoginController.java
@@ -14,15 +14,18 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import static utilities.UtilidadesJavaFX.*;
 
 /**
  * FXML Controller class
  *
- * @author Victor
+ * @author Victor e Ivan
  */
 public class FXMLLoginController implements Initializable {
+
+    private String privateKey;
 
     /**
      * Initializes the controller class.
@@ -30,33 +33,40 @@ public class FXMLLoginController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         System.out.println("Entramos en intialize Login");
-    }    
-    
+        //Enviar solicitud de clave de cifrado
+    }
+
     @FXML
     private void handleButtonLogin(ActionEvent event) {
         System.out.println("EN LOGIN");
-        
+        boolean registro = ((PasswordField) DesktopApp.getStage().getScene().lookup("#Login_rePass")).isVisible();
+        String log = ((TextField) DesktopApp.getStage().getScene().lookup("#Login_User")).getText();
+        String pass = ((PasswordField) DesktopApp.getStage().getScene().lookup("#Login_Pass")).getText();
+        String pass2 = ((PasswordField) DesktopApp.getStage().getScene().lookup("#Login_rePass")).getText();
+        if (!log.isEmpty() && !pass.isEmpty() && (!registro || (registro && !pass2.isEmpty()))) {
+            
+        }
     }
-    
+
     @FXML
     private void handleButtonRegister(MouseEvent event) {
-        PasswordField segundaPass=(PasswordField)DesktopApp.getStage().getScene().lookup("#Login_rePass");
-        Button boton=(Button)DesktopApp.getStage().getScene().lookup("#BotonLogin");
-        if(!segundaPass.isVisible()){
+        PasswordField segundaPass = (PasswordField) DesktopApp.getStage().getScene().lookup("#Login_rePass");
+        Button boton = (Button) DesktopApp.getStage().getScene().lookup("#BotonLogin");
+        if (!segundaPass.isVisible()) {
             System.out.println("A REGISTER");
             segundaPass.setManaged(true);
             segundaPass.setVisible(true);
-            ((Label)event.getSource()).setText("Sign In");
+            ((Label) event.getSource()).setText("Sign In");
             boton.setText("SIGN UP");
-        }else{
+        } else {
             System.out.println("De vuelta a Login");
             segundaPass.setManaged(false);
             segundaPass.setVisible(false);
-            ((Label)event.getSource()).setText("Sign Up");
+            ((Label) event.getSource()).setText("Sign Up");
             boton.setText("SIGN IN");
         }
     }
-    
+
     @FXML
     private void handleButtonBack(ActionEvent event) {
         System.out.println("EN BACK");
@@ -65,14 +75,14 @@ public class FXMLLoginController implements Initializable {
         loader = new FXMLLoader(getClass().getResource("/fxml/FXMLMenuPrincipal.fxml"), bundle);
         changeSceneRoot(loader, DesktopApp.getStage());
     }
-    
+
     @FXML
-    private void handleOnMouseOver(MouseEvent event){
+    private void handleOnMouseOver(MouseEvent event) {
         gestionPunteroRatonOver();
     }
-    
+
     @FXML
-    private void handleOnMouseOut(MouseEvent event){
+    private void handleOnMouseOut(MouseEvent event) {
         gestionPunteroRatonOut();
     }
 }
