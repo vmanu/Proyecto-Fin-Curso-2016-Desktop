ece2cc215797004f626f41b64e02f307ec21f4e3
Victor <Victor@LAPTOP-LMMJ4DI2>
2016-05-30 11:41:41 +0200

    14:44

---
+++ DataPPTGame/src/main/java/constantes/conexion/ConstantesConexion.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constantes.conexion;
+
+/**
+ *
+ * @author Victor
+ */
+public class ConstantesConexion {
+    public static String SERVICIO_WEBSOCKET="ws://localhost:8080/ServerPPTGame/ppt?user=";//"ws://servidorpptgame.jelastic.cloudhosted.es/ServerPPTGame/ppt?user=";
+    public static String URL_SERVIDOR="http://localhost:8080/ServerPPTGame/";//"http//servidorpptgame.jelastic.cloudhosted.es/ServerPPTGame/"
+    public static String URL_SEGURIDAD="seguridad";
+    public static String URL_LOGIN="login";
+    public static String URL_SERVLET_DB="ServletDB?op=";
+    public static String URL_GET_BY_VICTORIES="getByVictories";
+    public static String URL_GET_BY_AVERAGE="getByAverage";
+    public static String URL_GET_BY_ROUNDS="getByRounds";
+    public static String URL_AGREGAR_USUARIO="put";
+    public static String UTF_8="UTF-8";
+    public static String USER="user";
+    public static String SI="SI";
+    public static String NO="NO";
+    public static String CLAVE_HASHEADA="claveHasheada";
+    public static String COMPLEMENTO_HASHEADO="complementoHasheado";
+    public static String PREFERENCIAS_CONECTADO="conectado";
+    public static String PREFERENCIAS_NOMBRE_CONECTADO="nombreConectadoLogin";
+}
