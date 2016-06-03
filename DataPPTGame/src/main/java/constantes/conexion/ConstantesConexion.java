2173c1639910ed69dfb33cfe88d3c337fbb6c369
vmanu <vmanuoveuh@hotmail.com>
2016-06-03 08:49:23 +0200

    17:42

--- DataPPTGame/src/main/java/constantes/conexion/ConstantesConexion.java
+++ DataPPTGame/src/main/java/constantes/conexion/ConstantesConexion.java
@@ -10,21 +10,21 @@ package constantes.conexion;
  * @author Victor
  */
 public class ConstantesConexion {
-    public static String SERVICIO_WEBSOCKET="ws://localhost:8080/ServerPPTGame/ppt?user=";//"ws://servidorpptgame.jelastic.cloudhosted.es/ServerPPTGame/ppt?user=";
-    public static String URL_SERVIDOR="http://localhost:8080/ServerPPTGame/";//"http//servidorpptgame.jelastic.cloudhosted.es/ServerPPTGame/"
-    public static String URL_SEGURIDAD="seguridad";
-    public static String URL_LOGIN="login";
-    public static String URL_SERVLET_DB="ServletDB?op=";
-    public static String URL_GET_BY_VICTORIES="getByVictories";
-    public static String URL_GET_BY_AVERAGE="getByAverage";
-    public static String URL_GET_BY_ROUNDS="getByRounds";
-    public static String URL_AGREGAR_USUARIO="put";
-    public static String UTF_8="UTF-8";
-    public static String USER="user";
-    public static String SI="SI";
-    public static String NO="NO";
-    public static String CLAVE_HASHEADA="claveHasheada";
-    public static String COMPLEMENTO_HASHEADO="complementoHasheado";
-    public static String PREFERENCIAS_CONECTADO="conectado";
-    public static String PREFERENCIAS_NOMBRE_CONECTADO="nombreConectadoLogin";
+    public static final String SERVICIO_WEBSOCKET="ws://localhost:8080/ServerPPTGame/ppt?user=";//"ws://servidorpptgame.jelastic.cloudhosted.es/ServerPPTGame/ppt?user=";
+    public static final String URL_SERVIDOR="http://localhost:8080/ServerPPTGame/";//"http//servidorpptgame.jelastic.cloudhosted.es/ServerPPTGame/"
+    public static final String URL_SEGURIDAD="seguridad";
+    public static final String URL_LOGIN="login";
+    public static final String URL_SERVLET_DB="ServletDB?op=";
+    public static final String URL_GET_BY_VICTORIES="getByVictories";
+    public static final String URL_GET_BY_AVERAGE="getByAverage";
+    public static final String URL_GET_BY_ROUNDS="getByRounds";
+    public static final String URL_AGREGAR_USUARIO="put";
+    public static final String UTF_8="UTF-8";
+    public static final String USER="user";
+    public static final String SI="SI";
+    public static final String NO="NO";
+    public static final String CLAVE_HASHEADA="claveHasheada";
+    public static final String COMPLEMENTO_HASHEADO="complementoHasheado";
+    public static final String PREFERENCIAS_CONECTADO="conectado";
+    public static final String PREFERENCIAS_NOMBRE_CONECTADO="nombreConectadoLogin";
 }
